f18e461b7d4e1fd734c447822227a1abfb1fac78
majianjia <majianjia@live.com>
2019-07-03 14:22:39 +0100

    Update main.py

--- examples/auto_test/main.py
+++ examples/auto_test/main.py
@@ -25,46 +25,19 @@ from nnom_utils import *
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-model_name = 'keras_mnist_trained_model.h5'
-save_dir = os.path.join(os.getcwd(), 'saved_models')
+save_dir = 'keras_mnist_trained_model.h5'
 
+"""
 def build_model(x_shape):
     inputs = Input(shape=x_shape)
     x = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), padding='valid')(inputs)
     #x = BatchNormalization()(x)
     x = MaxPool2D((2, 2), strides=(2, 2), padding="same")(x)
 
-    """
     x = DepthwiseConv2D(kernel_size=(3, 3), strides=(1, 1), padding="valid")(x)
     x = BatchNormalization()(x)
 
-    
-    x = Cropping2D(cropping=((3,2),(3,1)))(x)
-    x = UpSampling2D(size=(2,2))(x)
-    x = ZeroPadding2D(padding=((1, 2), (3, 4)))(x)
-    """
-    """
-    x = Conv2D(24, kernel_size=(3, 3), strides=(1, 1), padding="same")(x)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
-    x = MaxPool2D((2, 2), strides=(2, 2), padding="same")(x)
-
-   
-    x = Conv2D(24, kernel_size=(3, 3), strides=(1, 1), padding="same")(x)
-    x = BatchNormalization()(x)
-
-    x = Conv2D(24, kernel_size=(3, 3), strides=(1, 1), padding="same")(x)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
-
-
-    x1 = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding="same")(x)
-    x2 = Conv2D(32, kernel_size=(5, 5), strides=(1, 1), padding="same")(x)
-    x3 = Conv2D(32, kernel_size=(1, 1), strides=(1, 1), padding="same")(x)
-
-    x = Concatenate(axis=-1)([x1, x2, x3])
-    
-    """
+  
     x = Conv2D(32, kernel_size=(3,3), strides=(1,1), padding="valid")(x)
     #x = BatchNormalization()(x)
     x = ReLU()(x)
@@ -93,14 +66,8 @@ def build_model(x_shape):
     return model
 
 def train(model, x_train, y_train, x_test, y_test, batch_size=64, epochs=50):
-
-    # Save model and weights
-    if not os.path.isdir(save_dir):
-        os.makedirs(save_dir)
-    model_path = os.path.join(save_dir, model_name)
-
     # save best
-    checkpoint = ModelCheckpoint(filepath=model_path,
+    checkpoint = ModelCheckpoint(filepath=save_dir,
             monitor='val_acc',
             verbose=0,
             save_best_only='True',
@@ -119,6 +86,87 @@ def train(model, x_train, y_train, x_test, y_test, batch_size=64, epochs=50):
     K.clear_session()
 
     return history
+"""
+
+def dense_block(x, k):
+    x1 = Conv2D(k, kernel_size=(3, 3), strides=(1,1), padding="same")(x)
+    x1 = BatchNormalization()(x1)
+    x1 = ReLU()(x1)
+
+    x2 = concatenate([x, x1],axis=-1)
+    x2 = Conv2D(k, kernel_size=(3, 3), strides=(1,1), padding="same")(x2)
+    x2 = BatchNormalization()(x2)
+    x2 = ReLU()(x2)
+
+    x3 = concatenate([x, x1, x2],axis=-1)
+    x3 = Conv2D(k, kernel_size=(3, 3), strides=(1,1), padding="same")(x3)
+    x3 = BatchNormalization()(x3)
+    x3 = ReLU()(x3)
+
+    x4 = concatenate([x, x1, x2, x3],axis=-1)
+    x4 = Conv2D(k, kernel_size=(3, 3), strides=(1,1), padding="same")(x4)
+    x4 = BatchNormalization()(x4)
+    x4 = ReLU()(x4)
+    
+    x5 = concatenate([x, x1, x2, x3, x4], axis=-1)
+    return x5
+
+def train(x_train, y_train, x_test, y_test, batch_size= 64, epochs = 100):
+
+    inputs = Input(shape=x_train.shape[1:])
+    x = Conv2D(12, kernel_size=(5, 5), strides=(1, 1), padding='same')(inputs)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    x = MaxPool2D((2,2), strides=(2,2))(x)
+
+    # dense block 1
+    x = dense_block(x, k=12)
+
+    # bottleneck
+    x = Conv2D(36, kernel_size=(1, 1), strides=(1, 1), padding="same")(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    x = MaxPool2D((2, 2), strides=(2, 2))(x)
+
+    # dense block 2
+    x = dense_block(x, k=12)
+
+    x = Conv2D(48, kernel_size=(3, 3), strides=(1, 1), padding="same")(x)
+    x = Dropout(0.3)(x)
+    x = Flatten()(x)
+    x = Dense(10)(x)
+
+    predictions = Softmax()(x)
+
+    model = Model(inputs=inputs, outputs=predictions)
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    # save best
+    checkpoint = ModelCheckpoint(filepath=save_dir,
+            monitor='val_acc',
+            verbose=0,
+            save_best_only='True',
+            mode='auto',
+            period=1)
+    callback_lists = [checkpoint]
+
+    history = model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=2,
+              validation_data=(x_test, y_test),
+              shuffle=True, callbacks=callback_lists)
+
+    # free the session to avoid nesting naming while we load the best model after.
+    del model
+    K.clear_session()
+    return history
+
 
 def main():
     """
@@ -157,15 +205,15 @@ def main():
     generate_test_bin(x_test*127, y_test, name='mnist_test_data.bin')
 
     # build model
-    model = build_model(x_test.shape[1:])
+    #model = build_model(x_test.shape[1:])
 
     # train model
-    history = train(model, x_train, y_train, x_test, y_test, epochs=epochs)
+    #history = train(model, x_train, y_train, x_test, y_test, epochs=epochs)
+    train( x_train, y_train, x_test, y_test, epochs=epochs)
 
     # -------- generate weights.h (NNoM model) ----------
     # get the best model
-    model_path = os.path.join(save_dir, model_name)
-    model = load_model(model_path)
+    model = load_model(save_dir)
 
     # evaluate
     evaluate_model(model, x_test, y_test)
