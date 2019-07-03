6680f34d9ddd7dfb82e87107a9372656f60528d0
majianjia <majianjia@live.com>
2019-07-03 15:09:43 +0100

    travis

--- examples/auto_test/main.py
+++ examples/auto_test/main.py
@@ -27,25 +27,25 @@ from nnom_utils import *
 
 save_dir = 'keras_mnist_trained_model.h5'
 
-
 def build_model(x_shape):
     inputs = Input(shape=x_shape)
     x = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), padding='valid')(inputs)
-    #x = BatchNormalization()(x)
-    x = MaxPool2D((2, 2), strides=(2, 2), padding="same")(x)
-
-    x = DepthwiseConv2D(kernel_size=(3, 3), strides=(1, 1), padding="valid")(x)
     x = BatchNormalization()(x)
 
-  
     x = Conv2D(32, kernel_size=(3,3), strides=(1,1), padding="valid")(x)
-    #x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
     x = ReLU()(x)
     x = MaxPool2D((2, 2), strides=(2, 2), padding="same")(x)
     x = Dropout(0.2)(x)
 
     x = Conv2D(32, kernel_size=(3,3), strides=(1,1), padding="valid")(x)
-    #x = BatchNormalization()(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    x = MaxPool2D((2, 2), strides=(2, 2), padding="same")(x)
+    x = Dropout(0.2)(x)
+
+    x = Conv2D(32, kernel_size=(3,3), strides=(1,1), padding="valid")(x)
+    x = BatchNormalization()(x)
     x = ReLU()(x)
     x = MaxPool2D((2, 2), strides=(2, 2), padding="same")(x)
     x = Dropout(0.2)(x)
@@ -86,87 +86,6 @@ def train(model, x_train, y_train, x_test, y_test, batch_size=64, epochs=50):
     K.clear_session()
 
     return history
-"""
-
-def dense_block(x, k):
-    x1 = Conv2D(k, kernel_size=(3, 3), strides=(1,1), padding="same")(x)
-    x1 = BatchNormalization()(x1)
-    x1 = ReLU()(x1)
-
-    x2 = concatenate([x, x1],axis=-1)
-    x2 = Conv2D(k, kernel_size=(3, 3), strides=(1,1), padding="same")(x2)
-    x2 = BatchNormalization()(x2)
-    x2 = ReLU()(x2)
-
-    x3 = concatenate([x, x1, x2],axis=-1)
-    x3 = Conv2D(k, kernel_size=(3, 3), strides=(1,1), padding="same")(x3)
-    x3 = BatchNormalization()(x3)
-    x3 = ReLU()(x3)
-
-    x4 = concatenate([x, x1, x2, x3],axis=-1)
-    x4 = Conv2D(k, kernel_size=(3, 3), strides=(1,1), padding="same")(x4)
-    x4 = BatchNormalization()(x4)
-    x4 = ReLU()(x4)
-    
-    x5 = concatenate([x, x1, x2, x3, x4], axis=-1)
-    return x5
-
-def train(x_train, y_train, x_test, y_test, batch_size= 64, epochs = 100):
-
-    inputs = Input(shape=x_train.shape[1:])
-    x = Conv2D(12, kernel_size=(5, 5), strides=(1, 1), padding='same')(inputs)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
-    x = MaxPool2D((2,2), strides=(2,2))(x)
-
-    # dense block 1
-    x = dense_block(x, k=12)
-
-    # bottleneck
-    x = Conv2D(36, kernel_size=(1, 1), strides=(1, 1), padding="same")(x)
-    x = BatchNormalization()(x)
-    x = ReLU()(x)
-    x = MaxPool2D((2, 2), strides=(2, 2))(x)
-
-    # dense block 2
-    x = dense_block(x, k=12)
-
-    x = Conv2D(48, kernel_size=(3, 3), strides=(1, 1), padding="same")(x)
-    x = Dropout(0.3)(x)
-    x = Flatten()(x)
-    x = Dense(10)(x)
-
-    predictions = Softmax()(x)
-
-    model = Model(inputs=inputs, outputs=predictions)
-
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-
-    model.summary()
-
-    # save best
-    checkpoint = ModelCheckpoint(filepath=save_dir,
-            monitor='val_acc',
-            verbose=0,
-            save_best_only='True',
-            mode='auto',
-            period=1)
-    callback_lists = [checkpoint]
-
-    history = model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              verbose=2,
-              validation_data=(x_test, y_test),
-              shuffle=True, callbacks=callback_lists)
-
-    # free the session to avoid nesting naming while we load the best model after.
-    del model
-    K.clear_session()
-    return history
-"""
 
 def main():
     """
@@ -201,19 +120,18 @@ def main():
     x_train = x_train.astype('float32')/255
     print("data range", x_test.min(), x_test.max())
 
-    # generate binary dataset for NNoM validation, 0~1 -> 0~127, q7
-    generate_test_bin(x_test*127, y_test, name='mnist_test_data.bin')
-
     # build model
     model = build_model(x_test.shape[1:])
 
     # train model
-    history = train(model, x_train, y_train, x_test, y_test, epochs=epochs)
-    #train( x_train, y_train, x_test, y_test, epochs=epochs)
+    history = train(model, x_train, y_train, x_test.copy(), y_test.copy(), epochs=epochs)
 
     # -------- generate weights.h (NNoM model) ----------
     # get the best model
     model = load_model(save_dir)
+
+    # generate binary dataset for NNoM validation, 0~1 -> 0~127, q7
+    generate_test_bin(x_test*127, y_test, name='mnist_test_data.bin')
 
     # evaluate
     evaluate_model(model, x_test, y_test)
